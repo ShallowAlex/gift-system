f79cd755b0a97720bfe1f893b45baadd89713c9e
ShallowAlex <1033479146@qq.com>
2021-02-15 23:41:37 +0800

    v0.1_base_users_write_and_read

--- common/error.py
+++ common/error.py
@@ -4,10 +4,16 @@ class NotPathError(Exception):
     def __init__(self, message):
         self.message = message
 
+
 class FormatError(Exception):
     def __init__(self, message):
         self.message = message
 
+
 class NotFileError(Exception):
     def __init__(self, message):
+        self.message = message
+
+class UserExistsError(Exception):
+    def __init__(self,message):
         self.message = message
